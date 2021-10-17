d8e4338567715edaefe6f69fe4617598038c28a4
Bjoern Doebel <bjoern.doebel@gmail.com>
2021-10-17 23:18:47 +0200

    Reformat
    rustfmt run
    
    Signed-off-by: Bjoern Doebel <bjoern.doebel@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 extern crate sdl2;
 
-use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
 use std::time::Duration;
 
 enum Configuration {
     ScreenWidth = 800,
-    ScreenHeight = 600
+    ScreenHeight = 600,
 }
 
 fn main() {
@@ -17,16 +17,19 @@ fn main() {
     println!("Initialized SDL Library.");
 
     let video_subsystem = sdl_context.video().unwrap();
- 
-    let window = video_subsystem.window("rust-sdl2 demo",
-                                              Configuration::ScreenWidth as u32,
-                                              Configuration::ScreenHeight as u32)
+
+    let window = video_subsystem
+        .window(
+            "rust-sdl2 demo",
+            Configuration::ScreenWidth as u32,
+            Configuration::ScreenHeight as u32,
+        )
         .position_centered()
         .build()
         .unwrap();
- 
+
     let mut canvas = window.into_canvas().build().unwrap();
- 
+
     canvas.set_draw_color(Color::RGB(0, 255, 255));
     canvas.clear();
     canvas.present();
@@ -38,10 +41,11 @@ fn main() {
         canvas.clear();
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit {..} |
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    break 'running
-                },
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
                 _ => {}
             }
         }
